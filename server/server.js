ac10329fdf6e01b268511cc36c760f58178349b9
Yashvin Vedanaparti <yvedanaparti@gmail.com>
2024-07-22 16:02:59 -0400

    typo with port env var - had it in lowercase

--- server/server.js
+++ server/server.js
@@ -65,6 +65,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist', 'index.html'));
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port);
 console.log(`Server listening on port ${port}`);
